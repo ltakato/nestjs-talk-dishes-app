a3e87a34c13f65a2aba62176cde084b2200c906d
takato <ltakato@hotmail.com>
2021-09-04 15:06:52 -0300

    feat: classe de dish

---
+++ src/dish.ts
@@ -0,0 +1,10 @@
+export class Dish {
+    Id: number;
+    Nome: string;
+    Imagem: string;
+    Descricao: string;
+    Categoria: string;
+    Preco: number;
+    CriadoEm: Date;
+    AtualizadoEm: Date;
+}
