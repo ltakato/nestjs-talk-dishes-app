94eb26ce18657bdced808b72c3ce418dc7dcabdb
takato <ltakato@hotmail.com>
2021-09-14 19:57:09 -0300

    terminar crud

--- src/dishes/dishes.controller.ts
+++ src/dishes/dishes.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Dish } from 'src/dish';
 
 import { CreateDishDto } from '../create-dish-dto';
 import { DishesService } from './dishes.service';
@@ -10,5 +11,24 @@ export class DishesController {
     @Post()
     create(@Body() createDishDto: CreateDishDto) {
         this.dishesService.criar(createDishDto);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: string, @Body() createDishDto: CreateDishDto) {
+        const idNumber: number = +id;
+        
+        this.dishesService.atualizar(idNumber, createDishDto)
+    }
+
+    @Get()
+    getAll(): Dish[] {
+        return this.dishesService.obterTodos();
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string) {
+        const idNumber: number = +id;
+        
+        this.dishesService.excluir(idNumber);
     }
 }
--- src/dishes/dishes.service.ts
+++ src/dishes/dishes.service.ts
@@ -3,17 +3,37 @@ import { Injectable } from '@nestjs/common';
 import { Dish } from '../dish';
 import { CreateDishDto } from '../create-dish-dto';
 
-const dishes: Dish[] = [];
+let dishes: Dish[] = [];
 
 @Injectable()
 export class DishesService {
     criar(createDishDto: CreateDishDto): void {
         const newDish = new Dish(createDishDto);
 
-        newDish.Id = dishes.length;
+        newDish.Id = Date.now();
         newDish.CriadoEm = new Date();
         newDish.AtualizadoEm = new Date();
 
         dishes.push(newDish);
+    }
+
+    atualizar(id: number, dishDto: CreateDishDto) {
+        const currentDish = dishes.find(dish => dish.Id === id);
+        const updatedDishIndex = dishes.findIndex(dish => dish.Id === id);
+
+        dishes[updatedDishIndex] = { 
+            ...dishDto,
+            Id: id,
+            CriadoEm: currentDish.CriadoEm,
+            AtualizadoEm: new Date()
+        };
+    }
+
+    excluir(id: number) {
+       dishes = dishes.filter(dish => dish.Id !== id);
+    }
+
+    obterTodos(): Dish[] {
+        return dishes;
     }
 }
